b4aabd5a487f917d1c55c549eb1bb24a0a7804c1
Willian Ferreira <williannfferreira@gmail.com>
2020-11-11 13:52:04 -0300

    authcontroller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    /**
+     *
+     */
+    public function register(Request $request)
+    {
+    }
+
+    public function logout(Request $request)
+    {
+    }
+
+    public function generalLogin(Request $request)
+    {
+    }
+
+    public function clientLogin(Request $request)
+    {
+    }
+
+    public function driverLogin(Request $request)
+    {
+    }
+
+    private function login(Request $request)
+    {
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('auth')->group(function () {
+    Route::post('register', 'App\Http\Controllers\AuthController@register');
+    Route::post('login', 'App\Http\Controllers\AuthController@generalLogin');
+    Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+
+    // Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+    // Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+});
